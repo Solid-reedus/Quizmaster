006018789cfb5fba2e05806d93f66056009f780d
Solid-reedus <75674268+Solid-reedus@users.noreply.github.com>
2023-09-11 11:47:52 +0200

    added sdl2 and sdl_image

---
+++ Quizmaster/Main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "SDL.h"
+
+int const SCREEN_WIDTH = 800;
+int const SCREEN_HEIGHT = 500;
+
+
+// initialization function will initialize methods, values and ect 
+// if it isnt able to initialize all of these things it will return a false
+// and will close the program
+bool Init();
+
+// this function will
+bool LoadMedia();
+
+// close function will destroy all memory that has been allocated 
+void Close();
+
+
+
+bool Init()
+{
+
+}
+
+
+
+void Close()
+{
+
+}
+
+
+
+int main(int argc, char* args[])
+{
+
+
+	while (true)
+	{
+		printf("aaa \n ");
+	}
+
+
+	return 0;
+}
