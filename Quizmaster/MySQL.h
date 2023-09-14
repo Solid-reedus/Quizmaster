688085e65f2d4d9238eab2ac579f9e18265c79e5
Solid-reedus <75674268+Solid-reedus@users.noreply.github.com>
2023-09-14 16:28:55 +0200

    made sql branch

--- Quizmaster/MySQL.h
+++ Quizmaster/MySQL.h
@@ -1,1 +1,37 @@
-#pragma once
+#ifndef MYSQL
+#define MYSQL
+
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+class MySQL
+{
+	public:
+
+	MySQL();
+	~MySQL();
+
+	void Free();
+
+	private:
+
+	bool ConnectToDb();
+
+
+	const std::string db_host = "localhost";
+	const std::string db_user = "root";
+	const std::string db_password = "";
+	const std::string db_name = "quizmaster";
+
+	sql::mysql::MySQL_Driver* driver;
+	sql::Connection* con;
+
+
+};
+
+
+
+#endif
